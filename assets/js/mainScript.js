4360b55a2fbdb351afab52f445185dbf6a6553c1
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-26 19:18:48 +0400

    feat: add project shell

---
+++ assets/js/mainScript.js
@@ -0,0 +1,17 @@
+const generatedContent = document.getElementById('body')
+
+generatedContent.innerHTML = mainPageContent()
+
+function mainPageContent() {
+    const renderMainPageContent =
+        `<main id="main">
+            <a href="https://github.com/Hacking-NASSA-with-HTML/Array_iteration_cheatsheet"
+                target="_blank"><img width="149" height="149"
+                src="./assets/img/fork_me_left_red.png"
+                style="position: fixed; top:0; left:0" alt="Fork me on GitHub">
+            </a>
+            <h1 class="titleH1">Play with nodejs</h1>
+            <p class="paragraph">See results in the Console</p>
+    </main>`
+    return renderMainPageContent
+}
