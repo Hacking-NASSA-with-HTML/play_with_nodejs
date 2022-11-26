777ba75a4c5957457edd400a28e8042849a7f065
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-26 22:41:15 +0400

    feat: add cap for real value hidden in .gitignore file

--- assets/js/pathToNames.js
+++ assets/js/pathToNames.js
@@ -1,6 +1,7 @@
-import { secret_Name } from "./secret.js";
+// import { secret_Name } from "./secret.js";
 
-export const secretName = secret_Name
+// export const secretName = secret_Name
+export const secretName = 'Real Value is hidden in .gitignore file'
 
 export const John = 'John'
 export const Maria = 'Maria'
