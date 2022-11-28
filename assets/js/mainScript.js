b3946e5bcec8bcf169a4ed9024e7c32bf0443424
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-28 17:53:22 +0400

    feat: add rotating sphere with skills list

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -2,24 +2,25 @@ import { John } from "./pathToNames.js";
 import { Maria } from "./pathToNames.js";
 import { secretName } from "./pathToNames.js";
 
-const generatedContent = document.getElementById('body')
+const generatedContent = document.getElementById('containerForGeneratedContent')
 
 generatedContent.innerHTML = mainPageContent()
 
 function mainPageContent() {
     const renderMainPageContent =
-        `<main id="main">
+        `<section id="mainSection">
             <a href="https://github.com/Hacking-NASSA-with-HTML/play_with_nodejs"
                 target="_blank"><img width="149" height="149"
                 src="./assets/img/fork_me_left_red.png"
                 style="position: fixed; top:0; left:0" alt="Fork me on GitHub">
             </a>
-            <h1 class="titleH1">Play with nodejs</h1>
+            <h1 class="titleH1">Code For Joy</h1>
             <p class="paragraph">See results in the Console</p>
             <p class="paragraph">${John}</p>
             <p class="paragraph">${Maria}</p>
             <p class="paragraph">secret name is: ${secretName}</p>
-    </main>`
+        </section>
+        `
     return renderMainPageContent
 }
 
